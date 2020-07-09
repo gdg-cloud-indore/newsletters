4962fa4cdabc9c2ca1ca18aca0fc41c38c59a484
Udit Sen <udisen97@gmail.com>
2020-07-10 03:11:12 +0530

    links added

--- assets/archives.js
+++ assets/archives.js
@@ -1,49 +1,49 @@
 const archives = [{
         category: "Newsletter",
         title: "Google Cloud Learning Hub, Quantum Supremacy and much more...",
-        link: "",
+        link: "./newsletters/june2020.html",
         date: "June, 2020"
     },
     {
         category: "Newsletter",
         title: "Cloud Shell, Cloud AutoML and more...",
-        link: "",
+        link: "./newsletters/may2020.html",
         date: "May 2020"
     },
     {
         category: "Newsletter",
         title: "Devfest '19, Actions on Google, Cloud APIs, Cloud Pub/Sub and more...",
-        link: "",
+        link: "./newsletters/aug2019.html",
         date: "August 2019"
     },
     {
         category: "Newsletter",
         title: "Big Query, Google Cloud SQL, Cloud Dataproc and more...",
-        link: "",
+        link: "./newsletters/july2019.html",
         date: "July 2019"
     },
     {
         category: "Event Summary",
         title: "Google IO Extended '19",
-        link: "",
+        link: "./events/ioextended.html",
         date: "June 2019"
     },
     {
         category: "Newsletter",
         title: "Kotlin, Tensorflow & More",
-        link: "",
+        link: "./newsletters/june2019.html",
         date: "June 2019"
     },
     {
         category: "Newsletter",
         title: "Cloud Next, Anthos, Cloud Run, Upcoming Events & more...",
-        link: "",
+        link: "./newsletters/may2019.html",
         date: "May 2019"
     },
     {
         category: "Event Summary",
         title: "Cloud Next '19 Extended",
-        link: "",
+        link: "./events/nextextended.html",
         date: "May 2019"
     },
 
