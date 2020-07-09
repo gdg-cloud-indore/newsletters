83d358bcfc4a304bfb2d5cf588306b4c2c90785a
Udit Sen <udisen97@gmail.com>
2020-07-10 01:49:36 +0530

    Added archives from array

---
+++ assets/archives.js
@@ -0,0 +1,50 @@
+const archives = [{
+        category: "Newsletter",
+        title: "Google Cloud Learning Hub, Quantum Supremacy and much more...",
+        link: "",
+        date: "June, 2020"
+    },
+    {
+        category: "Newsletter",
+        title: "Cloud Shell, Cloud AutoML and more...",
+        link: "",
+        date: "May 2020"
+    },
+    {
+        category: "Newsletter",
+        title: "Devfest '19, Actions on Google, Cloud APIs, Cloud Pub/Sub and more...",
+        link: "",
+        date: "August 2019"
+    },
+    {
+        category: "Newsletter",
+        title: "Big Query, Google Cloud SQL, Cloud Dataproc and more...",
+        link: "",
+        date: "July 2019"
+    },
+    {
+        category: "Event Summary",
+        title: "Google IO Extended '19",
+        link: "",
+        date: "June 2019"
+    },
+    {
+        category: "Newsletter",
+        title: "Kotlin, Tensorflow & More",
+        link: "",
+        date: "June 2019"
+    },
+    {
+        category: "Newsletter",
+        title: "Cloud Next, Anthos, Cloud Run, Upcoming Events & more...",
+        link: "",
+        date: "May 2019"
+    },
+    {
+        category: "Event Summary",
+        title: "Cloud Next '19 Extended",
+        link: "",
+        date: "May 2019"
+    },
+
+]
--- assets/script.js
+++ assets/script.js
@@ -18,8 +18,26 @@ navbuttons.forEach(button => {
             }
         })
         button.classList.add('active-nav')
+        if (button.id === 'archives-nav') {
+            document.getElementById('archives').style.height = 'auto'
+        } else {
+            document.getElementById('archives').style.height = '75vh'
+        }
     })
 })
 
 //remove this line
-//document.getElementById('archives').scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" })
+// document.getElementById('archives').scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" })
+
+//Filling Archives into the div
+archives.forEach(archive => {
+    let archiveCode = document.createElement('div')
+    archiveCode.classList.add('item--container')
+    archiveCode.innerHTML = `
+    <div class="item--category category-${archive.category === 'Newsletter' ? "newsletter" : "event"}">
+        ${archive.category}
+    </div>
+    <div class="item--link">${archive.title}</div>
+    <div class="item--date">${archive.date}</div>`
+    document.getElementById('archives--container').appendChild(archiveCode)
+})
