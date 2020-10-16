ca7caa9fec66c23b8537178bf6cc8ceba24e3888
Shachita Jain <shachitajains@gmail.com>
2020-10-17 01:24:29 +0530

    admin component added

---
+++ admin/main.js
@@ -0,0 +1,22 @@
+const adminFiles=[
+    {
+        link: "../devfest-mailers/example-1.html",
+        title: "Example 1"
+    },
+    {
+        link: "../devfest-mailers/example-2.html",
+        title: "Example 2"
+    },
+    {
+        link: "../devfest-mailers/example-3.html",
+        title: "Example 3"
+    },
+    {
+        link: "../devfest-mailers/day-1.html",
+        title: "Day 1"
+    }
+]
+
+for (i in adminFiles){
+document.getElementById('admin-list').innerHTML+=`<li><a href=${adminFiles[i].link}>${adminFiles[i].title}</a></li>`
+}
