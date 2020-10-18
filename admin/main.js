73169ac8e482a2e4e56dbbbef8b459ba5f653394
Shachita Jain <shachitajains@gmail.com>
2020-10-18 07:10:17 +0530

    Link to the day 2 template updated

--- admin/main.js
+++ admin/main.js
@@ -14,6 +14,10 @@ const adminFiles=[
     {
         link: "../devfest-mailers/day-1.html",
         title: "Day 1"
+    },
+    {
+        link: "../devfest-mailers/day-2.html",
+        title: "Day 2"
     }
    
 ]
